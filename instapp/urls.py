ee123861acfcc60e6db406d6389c2f7db3ebcdbd
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-06 19:49:04 +0300

    This commit adds the Users and posts urls

--- instapp/urls.py
+++ instapp/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# The include function allows me to reference another URLconf.
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include(('users,urls', 'users'), namespace='users')),
+    path('', include(('posts.urls', 'posts'), namespace='posts')),
+
 ]
