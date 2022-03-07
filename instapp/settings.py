6327e4de5f6483a0439733050ca4cbc4a8a22268
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-07 11:45:10 +0300

    This commit changes the contents of the databases settings to postgres

--- instapp/settings.py
+++ instapp/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'instapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
+        'USER': 'postgres',
+        'PASSWORD':'xoxo',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'Africa/Nairobi'
 
 TIME_ZONE = 'UTC'
 
