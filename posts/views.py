5a3f89536aef361ca34274b8020e789be0aa292d
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-07 06:57:23 +0300

    This commit defines ac createpost view which a login is required and creates a class for New post view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
+from re import template
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from posts.models import Post
+from posts.forms import PostForm
+
+class createPostView(LoginRequiredMixin, CreateView):
+    '''
+    Create New Post View
+    '''
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+    context_object_name = 'form'
