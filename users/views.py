83e128585826476e95cd9a393c1e04ab4e9e4c9e
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-07 07:15:57 +0300

    This commit creates a SignUp view class

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
+from re import template
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import logout
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView, FormView, UpdateView
+from django.contrib.auth.models import User
+from users.forms import SignupForm
+from posts.models import Post
+from users.models import Profile
+
+class SignupView(FormView):
+    """
+    Signup View
+    """
+    template_name = 'users/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('users:login')
